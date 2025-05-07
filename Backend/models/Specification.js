4b1cba56fe856663090167b43ce03952607f12ff
Amit Kumar <ramit5752@gmail.com>
2025-05-07 17:44:23 +0530

    I changed to the specification page frontend to backend

---
+++ Backend/models/Specification.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const specificationSchema = new mongoose.Schema({
+  fileName: String,
+  fileType: String,
+  fileSize: Number,
+  fileData: Buffer,
+  uploadedById: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  uploadedByEmail: String,
+  category: { type: String, default: 'N/A' }, // ✅ Add this line
+});
+
+module.exports = mongoose.model('Specification', specificationSchema);
